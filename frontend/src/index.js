5133cc1387a3aee37f39261cceb366ed3bbbeeee
Roger <a127038349@gmail.com>
2025-08-22 22:01:30 +0800

    fix: 移動 import 到 index.js 並清理 index.html

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,37 +1,40 @@
+// src/index.js －－ 直接整份覆蓋
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-//import './index.css';
+
+// 全域樣式：放在這裡（不要放到 public/index.html）
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/style.css';         // 你的 ogani 套版/全域樣式
+import './styles/index.css';         // 你的自訂全域樣式
+// 若你想用「本地版」Font Awesome，取消下一行註解（同時請移除 index.html 的 CDN 連結）
+// import 'font-awesome/css/font-awesome.min.css';
+
+// App 與其他 providers
 import App from './App';
 import { Provider } from 'react-redux';
-import  store  from './store/index';
-// import { store } from './redux/store';
+import store from './store/index';
 import reportWebVitals from './reportWebVitals';
-import { UserProvider } from "./contexts/UserData"; // 0813 Wayne 需要 for User Login OK
-import { OrderProvider } from "./api/OrderContext"; // 0813 Wayne 需要 for User Login OK
-import 'bootstrap/dist/css/bootstrap.min.css'; // 引入 Bootstrap 樣式
-import './styles/style.css'; // 引入ogani套版樣式(尚未全部引入)
+import { UserProvider } from './contexts/UserData';
+import { OrderProvider } from './api/OrderContext';
 import { BrowserRouter } from 'react-router-dom';
 import { bindGetState } from './services/merchantApi';
 
-bindGetState(() => store.getState()); // 綁定 getState 函數到 merchantApi
+// 綁定 getState
+bindGetState(() => store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-   <React.StrictMode>
+  <React.StrictMode>
     <Provider store={store}>
       <UserProvider>
         <OrderProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-       </OrderProvider>
-      </UserProvider> 
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </OrderProvider>
+      </UserProvider>
     </Provider>
-   </React.StrictMode>
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
